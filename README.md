13a0354eb72e0045ab27e358c17cbb92f0be2aec
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-07-21 11:51:06 -0300

    Add google auth button

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Getting Started with Create React App
+# Desafio sensedia
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Para o app funcionar, crie um arquivo dentro da pasta `src/config` chamado `clientId.js` com o id da sua conta google.
+Ele deveria ficar com esse formato:
+
+```
+export default 'SEU_ID_AQUI.apps.googleusercontent.com';
+```
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import GoogleLogin from 'react-google-login';
+import clientId from './config/clientId';
+
+const responseGoogle = response => {
+  console.log(response);
+};
+
 function App() {
-  return <h1>Desafio sensedia</h1>;
+  return (
+    <GoogleLogin
+      clientId={clientId}
+      buttonText="Faça login com o google"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy="single_host_origin"
+    />
+  );
 }
 
 export default App;
